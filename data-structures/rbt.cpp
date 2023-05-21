5fd9959da2d8b0e42877cc0c606a7eeac189dcaf
Ashraf Adillah <ashraf.alif@gmail.com>
2023-05-21 18:56:07 +0700

    remove unnecessary functions and fix BST insertion function
    at this point I found out that at
    SHA:ada343d3ca8cc5e51b05365d98903d895bd4289a I'm a genius but forgot how
    to create a BST insertion function. (but somehow still not fixed here)

--- data-structures/rbt.cpp
+++ data-structures/rbt.cpp
@@ -8,26 +8,27 @@ struct RBT {
     int key, value, height, bf; 
     RBT *left, *right, *parent; 
     Colors color; 
-} *root;
+};
 
 RBT *node_init(int key, int value); 
-RBT *node_insert(RBT *node); 
+RBT *node_insert(RBT *root, RBT *node); 
 RBT *node_delete(int key); 
 RBT *tree_rotate_left(RBT *node); 
 RBT *tree_rotate_right(RBT *node); 
 RBT *tree_rotation(RBT *node); 
 RBT *tree_colorize(RBT *node); 
 bool *node_search(int key); 
-int node_bf_set(RBT *node); 
-int node_height_set(RBT *node); 
-int node_height_get(RBT *node); 
-int max (int a, int b); 
+void tree_print(RBT *root); 
 
 int main(void) {
-    node_insert(node_init(12,0)); 
-    node_insert(node_init(1,0)); 
-    node_insert(node_init(2,0)); 
+    RBT *root = NULL; 
+    root = node_insert(root, node_init(12, 0)); 
+    root = node_insert(root, node_init(1, 1)); 
+    //root = node_insert(root, node_init(2, 2)); 
+    //root = node_insert(root, node_init(10,3)); 
 
+    tree_print(root); 
+    
     return 0; 
 }
 
@@ -35,33 +36,34 @@ RBT *node_init(int key, int value) {
     RBT *r = (RBT*)malloc(sizeof(RBT)); 
     r->value = value; 
     r->key = key; 
-    r->left = r->right = r->parent = NULL; 
+    r->left = r->right = NULL; 
+    r->parent = r; 
     r->color = RED; 
 
     return r; 
 }
 
-RBT *node_insert(RBT *node) {
+RBT *node_insert(RBT *root, RBT *node) {
     if (!root) return node; 
 
-    //assert(root->parent != NULL); 
+    assert(root->parent != NULL); 
     if (node->key > root->key){
-        root->right = node_insert(node); 
+        root->right = node_insert(root->right, node); 
         root->right->parent = root; // maybe is equivalent to node->parent = root (?)g 
         assert(root == tree_rotation(root->right)); 
         return tree_rotation(root->right); 
     }
     else if (node->key < root->key) {
-        root->left = node_insert(node); 
+        root->left = node_insert(root->left, node); 
         root->left->parent = root; // maybe is equivalent to node->parent = root (?)g 
         assert(root == tree_rotation(root->left)); 
         return tree_rotation(root->left); 
     }
-    else return root; 
+    return root; 
 }
 
 RBT *tree_rotation(RBT *node) {
-    assert(node == NULL); 
+    assert(node != NULL); 
     if (node->color != node->parent->color) return node;
 
     RBT *gparent = node->parent->parent; 
@@ -72,18 +74,11 @@ RBT *tree_rotation(RBT *node) {
     return node->parent; 
 }
 
-int node_height_set(RBT *node) {
-    return node ? max(node_height_get(node->left), node_height_get(node->right)) + 1 : 0; 
+void tree_print(RBT *root) {
+    if (!root) return ; 
+
+    tree_print(root->left);
+    printf("[key: %d, val: %d]--", root->key, root->value); 
+    tree_print(root->right);
 }
 
-int node_height_get(RBT *node) {
-    return node ? node->height : 0; 
-}
-
-int node_bf_set(RBT *node) {
-    return node ? node_height_get(node->left) - node_height_get(node->right) : 0; 
-}
-
-int max(int a, int b) {
-    return a > b ? a : b; 
-}
